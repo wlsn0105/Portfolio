27c755197baf55d9712c216307ddc8a1adc6196e
wlsn0105 <jack13579@naver.com>
2024-07-16 22:12:05 +0900

    Fix: css 수정

---
+++ src/components/AboutMeSection/AboutMeSection.jsx
@@ -0,0 +1,8 @@
+
+export default function AboutMeSection() {
+    return (
+        <div className="aboutMeSection">
+            11
+        </div>
+    ) 
+}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,15 +2,18 @@ import React from "react"
 import './styles/Main.css'
 import IntroductionSection from '../IntroductionSection/IntroductionSection.jsx'
 import Header from "../Header/Header.jsx"
+import AboutMeSection from '../AboutMeSection/AboutMeSection.jsx'
 
 export default function Main() {
     
     return (
         <div>
             <Header />
-            <IntroductionSection>
-            </IntroductionSection>
-            다음 섹션
+            <div className="container">
+                <IntroductionSection>
+                </IntroductionSection>
+                <AboutMeSection />
+            </div>
         </div>
     )
 }
