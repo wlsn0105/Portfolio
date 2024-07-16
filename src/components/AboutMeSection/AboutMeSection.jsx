af2caf9e9210ede116c7a23b4da5f52ee30db3ad
wlsn0105 <jack13579@naver.com>
2024-07-17 00:00:06 +0900

    Feat: About 섹션 레이아웃 조정

--- src/components/AboutMeSection/AboutMeSection.jsx
+++ src/components/AboutMeSection/AboutMeSection.jsx
@@ -1,8 +1,24 @@
+import React from "react"
+import './styles/AboutMeSection.css'
 
 export default function AboutMeSection() {
     return (
         <div className="aboutMeSection">
-            11
+            <div className="aboutMeContainer">
+                <div className="containerHeader">
+                    <h2>
+                        ABOUT ME
+                    </h2>
+                </div>
+                <div className="containerBox">
+                    <div className="containerLeft">
+                        왼쪽
+                    </div>
+                    <div className="containerRight">
+                        오른쪽
+                    </div>
+                </div>
+            </div>
         </div>
     ) 
 }
--- src/components/IntroductionSection/IntroductionSection.jsx
+++ src/components/IntroductionSection/IntroductionSection.jsx
@@ -12,20 +12,20 @@ export default function IntroductionSection() {
                         <img src={myphoto} alt="myphoto" className="myphotoImg" />
                         <Introduction />
                     </div>
-                    <div className="scrollAnimation">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="2rem"
-                        height="2rem"
-                        color="#222"
-                        viewBox="0 0 24 24"
-                        >
-                        <path
-                            fill="currentColor"
-                            d="M11 19.5V7.914l-4.5 4.5L5.086 11L12 4.086L18.914 11L17.5 12.414l-4.5-4.5V19.5z"
-                        />
-                        </svg>
-                    </div>
+                        <div className="scrollAnimation">
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="2rem"
+                            height="2em"
+                            color="#222"
+                            viewBox="0 0 24 24"
+                            >
+                            <path
+                                fill="currentColor"
+                                d="M11 19.5V7.914l-4.5 4.5L5.086 11L12 4.086L18.914 11L17.5 12.414l-4.5-4.5V19.5z"
+                            />
+                            </svg>
+                        </div>
                 </div>
             </div>
         )
