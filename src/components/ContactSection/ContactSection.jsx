4dea3f679e7619bd9c842fcea084775a082fd186
wlsn0105 <jack13579@naver.com>
2024-07-22 16:25:53 +0900

    Feat: Contact 섹션 완료

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './styles/ContactSection.css'
 import myphoto from '../../assets/images/myphoto.JPG'
+import emailIcon from '../../assets/images/emailIcon.png'
+import phoneIcon from '../../assets/images/phoneIcon.png'
 
 export default function ContactSection() {
     return (
@@ -12,7 +14,20 @@ export default function ContactSection() {
                 <img src={myphoto} alt="myphoto" className="myphotoImg" />
                 <div className="containerText">
                     <h3>믿고 맡길 수 있는 개발자가 필요한가요?</h3>
+                    <span>팀원들과 소통하며 사용자의 관점을 생각하는 UX 개발자 박진우입니다.</span>
+                    <span>언제든 연락주세요!☺️</span>
+                    <div className="emailBox">
+                        <img src={emailIcon} alt="emailIcon" className="emailIconImg" />
+                        <span>pjw96929692@gmail.com</span>
+                    </div>
+                    <div className="phoneBox">
+                        <img src={phoneIcon} alt="phoneIcon" className="phoneIconImg" />
+                        <span>010.5743.9692</span>
+                    </div>
                 </div>
+            </div>
+            <div className="copyrightContainer">
+                <span className="copyrightText">COPYRIGHT 2024. Park Jinwoo All rights reserved</span>
             </div>
         </div>
     )
