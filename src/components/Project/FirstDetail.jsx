5142a529433540185b94fd4897e0f9b98f29839c
wlsn0105 <jack13579@naver.com>
2024-07-24 11:30:21 +0900

    Feat: 상세페이지 레이아웃 설계

--- src/components/Project/FirstDetail.jsx
+++ src/components/Project/FirstDetail.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import MoreHeader from '../Header/MoreHeader.jsx'
+import './styles/FirstDetail.css'
+import jooyajooya from '../../assets/images/ProjectSectionImages/jooyajooya.png'
 
 export default function FirstDetail() {
   return (
       <div>
         <MoreHeader />
-        <div>
-            <h1>Detail Page</h1>
-            <p>This is the detail page.</p>
+          <div className='firstDetailSection'>
+              <div className='detailContainer'>
+                <div className='detailBox'>
+                      <div className='detailTitle'>
+                          <img src={jooyajooya} alt="jooyajooya" className="jooyajooyaImg" />
+                        <h2>
+                            &nbsp; 음성 프로토콜 상담 플랫폼(외주)
+                        </h2>
+                      </div>
+                      <div>
+                          ss
+                      </div>
+                </div>
+            </div>
         </div>
     </div>
   );
