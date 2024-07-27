32f931b894b673f507387777f70f726b5c6587dc
wlsn0105 <jack13579@naver.com>
2024-07-27 21:27:13 +0900

    Feat: 두번째 프로젝트 복사

--- src/components/Project/ProjectSecondSection/ProjectSecondSection.jsx
+++ src/components/Project/ProjectSecondSection/ProjectSecondSection.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import './styles/ProjectSecondSection.css'
+import imacFirst from '../../../assets/images/ProjectSectionImages/imacFirst.png'
+import jooyajooya from '../../../assets/images/ProjectSectionImages/jooyajooya.png'
 
 export default function ProjectSecondSection() {
+
+    const firstDetail = () => {
+        window.open('/Portfolio/firstDetail', '_blank');
+        };
+
     return (
         <div className="projectSecondSection">
-            ddd
+            <div className="projectFirstContainer">
+                    <div className="firstImgBox">
+                        <img src={imacFirst} alt="imacFirst" className="imacFirstImg" />
+                    </div>
+                    <div className="explainBox">
+                            <img src={jooyajooya} alt="jooyajooya" className="jooyajooyaImg" />
+                        <h3>
+                            음성 프로토콜 상담 플랫폼(외주)
+                        </h3>
+                        <span>노년층을 타겟으로 한 음성 상담 플랫폼 웹앱 사이트입니다.</span>
+                        <span>업체와 직접 소통하면서 색상부터 시작해서 위계질서를 고려하며 UI,</span>
+                        <span>사용자의 관점을 고려하며 UX를 설계하였습니다.</span>
+                        <span>웹앱 디자인 및 개발 작업을 진행하며 배포까지 진행하였습니다.</span>
+                        <div className="buttonGroup">
+                            <button onClick={firstDetail}>More</button>
+                            <a href="https://www.figma.com/design/XXza63VFx7ITacDHIbjzl6/IMPL?node-id=0-1&t=hQ3XIBEF5wvElIKw-1" target="_blank" rel="noreferrer">
+                                <button className="moreFigma">
+                                    Figma
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div> 
         </div>
     )
 }
