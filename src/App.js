e886313c4914bd135e39197ffeaa591bac79a50d
wlsn0105 <jack13579@naver.com>
2024-07-29 23:57:20 +0900

    Fix: 최종 수정

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import { Route, Routes } from "react-router-dom";
 import FirstDetail from "./components/Project/ProjectFirstSection/FirstDetail.jsx";
 import SecondDetail from "./components/Project/ProjectSecondSection/SecondDetail.jsx";
 import ThirdDetail from "./components/Project/ProjectThirdSection/ThirdDetail.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
     return (
         <div>
             {/* <Main /> */}
-            <Routes>
-                <Route path="/MyPortfolio" element={<Main />} />
-                <Route path="/MyPortfolio/firstDetail" element={<FirstDetail />} />
-                <Route path="/MyPortfolio/secondDetail" element={<SecondDetail />} />
-                <Route path="/MyPortfolio/thirdDetail" element={<ThirdDetail />} />
-            </Routes>
+            <Router>
+                <Routes>
+                    <Route path="/MyPortfolio" element={<Main />} />
+                    <Route path="/MyPortfolio/firstDetail" element={<FirstDetail />} />
+                    <Route path="/MyPortfolio/secondDetail" element={<SecondDetail />} />
+                    <Route path="/MyPortfolio/thirdDetail" element={<ThirdDetail />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
